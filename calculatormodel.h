0d506e646b3c15240015d1f026a01a273cfd9cba
Marc Rousavy <mrousavy@users.noreply.github.com>
2017-09-22 18:14:17 +0200

    Add Model

--- calculatormodel.h
+++ calculatormodel.h
@@ -6,6 +6,12 @@ class CalculatorModel
 {
 public:
     CalculatorModel();
+
+    // Calculate Sum of all given input parameters
+    template <class T> T Sum(int count, T ...);
+
+    // Calculate Absolute value of an integer
+    int Absolute(int value);
 };
 
 #endif // CALCULATORMODEL_H
