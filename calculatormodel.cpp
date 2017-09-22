2196a3dc2783038cbca4d3fd2e829a9f9752ad99
Marc Rousavy <mrousavy@users.noreply.github.com>
2017-09-22 18:14:24 +0200

    Implement Calculator Model

--- calculatormodel.cpp
+++ calculatormodel.cpp
@@ -1,6 +1,30 @@
 #include "calculatormodel.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <math.h>
+using namespace std;
 
 CalculatorModel::CalculatorModel()
 {
 
+}
+
+template <class T>
+T CalculatorModel::Sum(int count, T ...)
+{
+    va_list params; // Parameter list
+    T total = 0;
+
+    va_start(params, count); // Requires the last fixed parameter (to get the address)
+    for(int i = 0; i < count; i++) { // iterate through each param
+        total += va_arg(params, T); // Add to total
+    }
+    va_end(params); // End argument list
+    return total;
+}
+
+
+int CalculatorModel::Absolute(int value)
+{
+    return abs(value);
 }
